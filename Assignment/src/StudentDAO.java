7636ec339fc6f2c7777b335ca3242fb8ece4e880
Piotr Zadka <piotr.zadka@gmail.com>
2018-01-09 18:17:12 +0000

    Registration finished

--- Assignment/src/StudentDAO.java
+++ Assignment/src/StudentDAO.java
@@ -12,13 +12,13 @@ public class StudentDAO {
 
     /**
      * This method connects to our database
+     *
      * @return connection to database is made
      */
     public Connection getDBConnection() {
         try {
             Class.forName("org.sqlite.JDBC");
-        }
-        catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             System.out.println(e.getMessage());
         }
         try {
@@ -26,8 +26,7 @@ public class StudentDAO {
             dbConnection = DriverManager.getConnection(dbURL);
             s = dbConnection.createStatement();
 
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return dbConnection;
@@ -36,34 +35,34 @@ public class StudentDAO {
     /**
      * This method closes connection to database
      */
-    public void closeConnection(){
-        try{
-            if(s!=null){
+    public void closeConnection() {
+        try {
+            if (s != null) {
                 s.close();
             }
-            if(dbConnection!=null){
+            if (dbConnection != null) {
                 dbConnection.close();
             }
 
-        }
-        catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     /**
      * This method retrieves all students in database
+     *
      * @return Array List of all students in database
      */
-    public  ArrayList<Student> getAllStudents(){
+    public ArrayList<Student> getAllStudents() {
         Connection dbConnection = null;
         Statement statement = null;
         ResultSet resultset = null;
         String query = "SELECT * FROM students;";
         Student temp = null;
-        ArrayList<Student>allStudents = new ArrayList<>();
+        ArrayList<Student> allStudents = new ArrayList<>();
 
-        try{
+        try {
             dbConnection = getDBConnection();
             statement = dbConnection.createStatement();
             resultset = statement.executeQuery(query);
@@ -74,17 +73,16 @@ public class StudentDAO {
                 String dob = resultset.getString("DOB");
                 String address = resultset.getString("Address");
                 String postcode = resultset.getString("Postcode");
-                int  studentNumber = resultset.getInt("StudentNumber");
+                int studentNumber = resultset.getInt("StudentNumber");
                 String courseTitle = resultset.getString("CourseTitle");
                 String startDate = resultset.getString("StartDate");
                 Float bursary = resultset.getFloat("Bursary");
                 String email = resultset.getString("Email");
 
-                temp = new Student(name,gender,dob,address,postcode,studentNumber,courseTitle,startDate,bursary,email);
+                temp = new Student(name, gender, dob, address, postcode, studentNumber, courseTitle, startDate, bursary, email);
                 allStudents.add(temp);
             }
-        }
-        catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         closeConnection();
@@ -93,18 +91,19 @@ public class StudentDAO {
 
     /**
      * This method inserts new Student object into our database
+     *
      * @param newStudent Student object
      * @return True if successful, False otherwise.
      * @throws SQLException SQL errors
      */
-    public boolean insertStu(Student newStudent) throws SQLException{
+    public boolean insertStu(Student newStudent) throws SQLException {
         Connection dbConnection = null;
         Statement statement = null;
 
         String query = "INSERT INTO students (Name, Gender, DOB, Address, Postcode, StudentNumber, CourseTitle, StartDate, Bursary, Email) " +
-                "VALUES ('"+newStudent.getName()+"','"+newStudent.getGender()+"','"+newStudent.getDob()+"','"+newStudent.getAddress()+"','"+newStudent.getPostcode()+"'" +
-                ","+newStudent.getStudentNumber()+",'"+newStudent.getCourseTitle()+"','"+newStudent.getStartDate()+"',"+newStudent.getBursary()+",'"+newStudent.getEmail()+"');";
-        System.out.println("\nQUERY 3: "+query);
+                "VALUES ('" + newStudent.getName() + "','" + newStudent.getGender() + "','" + newStudent.getDob() + "','" + newStudent.getAddress() + "','" + newStudent.getPostcode() + "'" +
+                "," + newStudent.getStudentNumber() + ",'" + newStudent.getCourseTitle() + "','" + newStudent.getStartDate() + "'," + newStudent.getBursary() + ",'" + newStudent.getEmail() + "');";
+        System.out.println("\nQUERY 3: " + query);
         try {
             dbConnection = getDBConnection();
             statement = dbConnection.createStatement();
@@ -114,8 +113,7 @@ public class StudentDAO {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             return false;
-        }
-        finally {
+        } finally {
             statement.close();
         }
         closeConnection();
@@ -124,20 +122,21 @@ public class StudentDAO {
 
     /**
      * This method deletes specific student based on provided ID
+     *
      * @param stuNumber Student ID number
      * @return True if successful, otherwise false.
      * @throws SQLException SQL errors
      */
-    public boolean deleteStu(int stuNumber) throws SQLException{
+    public boolean deleteStu(int stuNumber) throws SQLException {
         Connection dbConnection = null;
         Statement statement = null;
         ResultSet resultset = null;
         int rowCount = 0;
 
         // Firstly check if row exists and returns anything if so delete it if not return false and display message "Row doesn't exists"
-        String queryV = "SELECT COUNT (*) from students WHERE StudentNumber = '"+stuNumber+"';";
-        String query = "DELETE FROM students WHERE StudentNumber = '"+stuNumber+"';";
-        System.out.println("\nQUERY 5: "+query);
+        String queryV = "SELECT COUNT (*) from students WHERE StudentNumber = '" + stuNumber + "';";
+        String query = "DELETE FROM students WHERE StudentNumber = '" + stuNumber + "';";
+        System.out.println("\nQUERY 5: " + query);
         try {
             dbConnection = getDBConnection();
             statement = dbConnection.createStatement();
@@ -145,10 +144,9 @@ public class StudentDAO {
             resultset.next();
             rowCount = resultset.getInt(1);
 
-            if(rowCount == 0){
+            if (rowCount == 0) {
                 return false;
-            }
-            else{
+            } else {
                 statement.executeUpdate(query);
             }
         } catch (SQLException e) {
@@ -161,19 +159,20 @@ public class StudentDAO {
 
     /**
      * This method returns specific student based on ID number
+     *
      * @param stuNumber Students ID number
      * @return Student Object
      * @throws SQLException SQL Error
      */
-    public Student getStudent(int stuNumber) throws SQLException{
+    public Student getStudent(int stuNumber) throws SQLException {
         Connection dbConnection = null;
         Statement statement = null;
         ResultSet resultset = null;
         Student temp = null;
 
-        String query = "SELECT * FROM students where StudentNumber = '"+stuNumber+"';";
-        System.out.println("\nQUERY 2: "+query);
-        try{
+        String query = "SELECT * FROM students where StudentNumber = '" + stuNumber + "';";
+        System.out.println("\nQUERY 2: " + query);
+        try {
             dbConnection = getDBConnection();
             statement = dbConnection.createStatement();
 
@@ -184,16 +183,15 @@ public class StudentDAO {
                 String dob = resultset.getString("DOB");
                 String address = resultset.getString("Address");
                 String postcode = resultset.getString("Postcode");
-                int  studentNumber = resultset.getInt("StudentNumber");
+                int studentNumber = resultset.getInt("StudentNumber");
                 String courseTitle = resultset.getString("CourseTitle");
                 String startDate = resultset.getString("StartDate");
                 Float bursary = resultset.getFloat("Bursary");
                 String email = resultset.getString("Email");
 
-                temp = new Student(name,gender,dob,address,postcode,studentNumber,courseTitle,startDate,bursary,email);
+                temp = new Student(name, gender, dob, address, postcode, studentNumber, courseTitle, startDate, bursary, email);
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         closeConnection();
@@ -203,30 +201,31 @@ public class StudentDAO {
 
     /**
      * This method updates specific students row in database
+     *
      * @param newStudent Provided student object
      * @return True if update is successful, otherwise false
      * @throws SQLException SQL errors
      */
-    public boolean updateStu(Student newStudent) throws SQLException{
+    public boolean updateStu(Student newStudent) throws SQLException {
         Connection dbConnection = null;
         Statement statement = null;
         ResultSet resultset = null;
         int rowCount = 0;
 
-        String queryV = "SELECT COUNT (*) from students WHERE StudentNumber = '"+newStudent.getStudentNumber()+"';";
+        String queryV = "SELECT COUNT (*) from students WHERE StudentNumber = '" + newStudent.getStudentNumber() + "';";
         String query = "UPDATE students SET " +
-                "Name = '"+newStudent.getName()+"',"+
-                "Gender = '"+newStudent.getGender()+"', "+
-                "DOB = '"+newStudent.getDob()+"', "+
-                "Address = '"+newStudent.getAddress()+"', "+
-                "Postcode = '"+newStudent.getPostcode()+"', "+
-                "CourseTitle = '"+newStudent.getCourseTitle()+"', "+
-                "StartDate = '"+newStudent.getStartDate()+"', "+
-                "Bursary = '"+newStudent.getBursary()+"', "+
-                "Email = '"+newStudent.getEmail()+"' "+
-                "WHERE StudentNumber = '"+newStudent.getStudentNumber()+"';";
+                "Name = '" + newStudent.getName() + "'," +
+                "Gender = '" + newStudent.getGender() + "', " +
+                "DOB = '" + newStudent.getDob() + "', " +
+                "Address = '" + newStudent.getAddress() + "', " +
+                "Postcode = '" + newStudent.getPostcode() + "', " +
+                "CourseTitle = '" + newStudent.getCourseTitle() + "', " +
+                "StartDate = '" + newStudent.getStartDate() + "', " +
+                "Bursary = '" + newStudent.getBursary() + "', " +
+                "Email = '" + newStudent.getEmail() + "' " +
+                "WHERE StudentNumber = '" + newStudent.getStudentNumber() + "';";
 
-        System.out.println("\nQUERY 4: "+query);
+        System.out.println("\nQUERY 4: " + query);
         try {
             dbConnection = getDBConnection();
             statement = dbConnection.createStatement();
@@ -234,13 +233,11 @@ public class StudentDAO {
             resultset.next();
             rowCount = resultset.getInt(1);
 
-            if(rowCount == 0){
+            if (rowCount == 0) {
                 return false;
-            }
-            else{
+            } else {
                 statement.executeUpdate(query);
             }
-
 
 
         } catch (SQLException e) {
@@ -253,6 +250,7 @@ public class StudentDAO {
 
     /**
      * This method handles user login in HTML form
+     *
      * @param username Form username
      * @param password Form password
      * @return true if connection is made, otherwise false
@@ -264,7 +262,7 @@ public class StudentDAO {
         ResultSet resultset = null;
 
         // Firstly check if user account exists and returns anything
-        String query = "SELECT * from users WHERE username = '"+username+"';";
+        String query = "SELECT * from users WHERE username = '" + username + "';";
 
         try {
             dbConnection = getDBConnection();
@@ -273,15 +271,14 @@ public class StudentDAO {
             resultset.next();
             String dbPassword = resultset.getString("password");
 
-            if(password.equals(dbPassword)){
+            if (password.equals(dbPassword)) {
+                closeConnection();
                 return true;
-            }
-            else{
+            } else {
                 closeConnection();
                 return false;
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
             return false;
         }
@@ -289,6 +286,7 @@ public class StudentDAO {
 
     /**
      * This method retrieves API key from database
+     *
      * @param username Form username provided by the user.
      * @return String format of API key
      * @throws SQLException SQL errors
@@ -300,7 +298,7 @@ public class StudentDAO {
         String dbApiKey = "";
 
         // Firstly check if user account exists and returns anything
-        String query = "SELECT apikey from users WHERE username = '"+username+"';";
+        String query = "SELECT apikey from users WHERE username = '" + username + "';";
 
         try {
             dbConnection = getDBConnection();
@@ -308,8 +306,7 @@ public class StudentDAO {
             resultset = statement.executeQuery(query);
             resultset.next();
             dbApiKey = resultset.getString("apikey");
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         statement.close();
@@ -320,6 +317,7 @@ public class StudentDAO {
 
     /**
      * This method check if api key is valid
+     *
      * @param key Api key provided by the user in REST post. ?key=
      * @return true if matches post key value, otherwise false.
      * @throws SQLException SQL errors
@@ -340,47 +338,58 @@ public class StudentDAO {
             dbApiKey = resultset.getString("apikey");
 
             if (StringKey.equals(dbApiKey)) {
+                statement.close();
+                resultset.close();
+                closeConnection();
                 return true;
             } else {
+                statement.close();
+                resultset.close();
+                closeConnection();
                 return false;
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             return false;
         }
-        finally {
-            statement.close();
-            resultset.close();
-            closeConnection();
-        }
     }
-    public boolean createAccount(String username, String password) throws SQLException{
+
+    public boolean createAccount(String username, String password) throws SQLException {
         Connection dbConnection = null;
         Statement statement = null;
         ResultSet resultset = null;
+        int rowCount = 0;
 
         //Generate random api key
         Random random = new Random();
         int randomApi = random.nextInt(900) + 100;
 
-
-        String query = "INSERT INTO users (username,password,apikey) VALUES ('"+username+"','"+password+"','"+randomApi+"');";
+        String checkExistsQuery = "SELECT COUNT (*) from users WHERE username = '" + username + "';";
+        String query = "INSERT INTO users (username,password,apikey) VALUES ('" + username + "','" + password + "','" + randomApi + "');";
 
         try {
             dbConnection = getDBConnection();
             statement = dbConnection.createStatement();
-            statement.executeUpdate(query);
-            return true;
-        }
-        catch (SQLException e) {
+            resultset = statement.executeQuery(checkExistsQuery);
+            resultset.next();
+            rowCount = resultset.getInt(1);
+
+            if (rowCount == 0) {
+                statement.executeUpdate(query);
+                statement.close();
+                resultset.close();
+                closeConnection();
+                return true;
+            } else {
+                statement.close();
+                resultset.close();
+                closeConnection();
+                return false;
+            }
+
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
             return false;
         }
-        finally {
-            statement.close();
-            resultset.close();
-            closeConnection();
-        }
     }
-
 }
